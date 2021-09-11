efe1b463f5379250cbdecdd67b9a789f25323db6
CecilebougmaIstic <cecile.bougma@etudiant.univ-rennes1.fr>
2021-09-11 20:46:45 +0200

    début class User & class Worker

--- src/main/java/jpa/Worker.java
+++ src/main/java/jpa/Worker.java
@@ -1,44 +1,14 @@
 package jpa;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 
-@Entity
+
 
 public class Worker extends User{
-	
 	private long id;
 	private String job;
+	private String bakRib;
 	
-	public Worker(long id, String job) {
-		super();
-		this.id = id;
-		this.job = job;
-	}
 
-	public Worker(String job) {
-		super();
-		this.job = job;
-	}
-	
-	@Id
-    @GeneratedValue
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getJob() {
-		return job;
-	}
-
-	public void setJob(String job) {
-		this.job = job;
-	}
 	
 	
 
