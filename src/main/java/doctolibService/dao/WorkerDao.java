8cffe1e8529a623cbaf5bae599ed499ee11c5058
CecilebougmaIstic <cecile.bougma@etudiant.univ-rennes1.fr>
2021-09-14 00:47:17 +0200

    méthode de recherche des types de rdv dans la classe WorkerDao

--- src/main/java/doctolibService/dao/WorkerDao.java
+++ src/main/java/doctolibService/dao/WorkerDao.java
@@ -1,6 +1,8 @@
 package doctolibService.dao;
 
 
+import java.util.List;
+
 import doctolibService.model.Worker;
 
 public class WorkerDao extends AbstractJpaDao <Long,Worker>{
@@ -9,6 +11,11 @@ public class WorkerDao extends AbstractJpaDao <Long,Worker>{
 		super(Worker.class);
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	/*Get a list*/
+  public List < Worker>getAllTypeOfAppointementsForAWorker(){
+	 String query ="select distinct K.typeAppointements from TypeOfAppointement as k";
+	 return this.entityManager.createQuery(query).getResultList();
+	}
 	
 }
